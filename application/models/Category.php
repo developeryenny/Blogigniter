a12da821cad8f021e10fbcc2edd524c9b954a68a
yenny <yennymirellapatino@gmail.com>
2019-07-31 12:49:51 +0200

    first commit

---
+++ application/models/Category.php
@@ -0,0 +1,5 @@
+<?php
+class Category extends CI_Model{
+    public $table = "categories";
+    public $table_id = "category_id";
+}
---
+++ application/views/admin/post_list.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="<?php echo base_url()?> assets/css/bootstrap.min.css">
+    <script src="<?php echo base_url()?>assets/js/jquery-3.4.1.min.js"></script>
+    <script src="<?php echo base_url()?>assets/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+<div class="container">
+    <h1>Titulos</h1>
+</div>
+
+</body>
+
+</html>
